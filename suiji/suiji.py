28c2d12ff37e67b6546266853048ee72049c3267
houaaaa <h2488969635@163.com>
2024-11-29 11:26:31 +0800

    Update suiji.py

--- suiji/suiji.py
+++ suiji/suiji.py
@@ -1,10 +1,10 @@
 from random import choice
 
 class RandomWalk:
-  def __init __(self, num_points=5000):
-  self.num_points = num_points
-  self.x_values = [0]
-  self.y_values = [0]
+  def __init__(self, num_points=5000):
+      self.num_points = num_points
+      self.x_values = [0]
+      self.y_values = [0]
 
 def fill_walk(self):
   while len(self.x_values) < sellf.num_points:
