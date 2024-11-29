874651decef5c0ba3fdd8b049ba9f19a685bd9a3
houaaaa <h2488969635@163.com>
2024-11-29 11:13:15 +0800

    Update visual.py

---
+++ suiji/visual.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+from suiji import RandomWalk
+
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.style.use('classic')
+ifg, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+ax.set_aspect('equal')
+
+while True:
+# 创建一个 RandomWalk 实例
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+    # 将所有的点都绘制出来
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+    edgecolors='none', s=1)
+    ax.set_aspect('equal')
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',s=100)
+    plt.show()
