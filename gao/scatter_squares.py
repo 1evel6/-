c977ca7d3948800bc87aaaab4bf701b27d5349f8
gaoshuai <g2077303270s@163.com>
2024-12-04 19:40:11 +0800

    Add python script for death_Valley_highs.low.py

--- gao/scatter_squares.py
+++ gao/scatter_squares.py
@@ -11,7 +11,6 @@ fig, ax = plt.subplots()
 
 ax.scatter(x_values, y_values, s=10)
 
-
 ax.scatter(2, 4, s=10)
 # 设置图题并给坐标轴加上标签
 ax.set_title("Square Numbers", fontsize=20)
@@ -30,9 +29,9 @@ ax.axis([0, 1100, 0,1_100_000])
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 ax.scatter(2, 4)
-plt.show()
+
 
 
 #如果要将绘图保存到文件中，而不是在 Matplotlib 查看器中显示它，可将 plt.show() 替
 #换为 plt.savefig()：
-#plt.savefig('squares_plot.png', bbox_inches='tight')
+plt.savefig('squares_plot.png', bbox_inches='tight')
